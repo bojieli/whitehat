0acff3f20dd9ecdbdf7d7868e01e5e2884f0e690
ctyi <1997cui@sina.com>
2015-04-29 21:53:06 +0800

    Change data structure of database, details are on the tower

--- whitehat.sql
+++ whitehat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2015-04-28 07:45:59
+-- Generation Time: 2015-04-29 13:49:13
 -- 服务器版本： 10.0.17-MariaDB-log
 -- PHP Version: 5.6.8
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `whitehat`
 --
+CREATE DATABASE IF NOT EXISTS `whitehat` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `whitehat`;
 
 -- --------------------------------------------------------
 
@@ -26,23 +28,37 @@ SET time_zone = "+00:00";
 -- 表的结构 `Loophole`
 --
 
+DROP TABLE IF EXISTS `Loophole`;
 CREATE TABLE IF NOT EXISTS `Loophole` (
   `id` int(11) NOT NULL,
   `username` varchar(10) COLLATE utf8_bin NOT NULL,
   `studentnumber` varchar(20) COLLATE utf8_bin NOT NULL,
+  `anonymous` tinyint(1) NOT NULL DEFAULT '0',
+  `gender` int(5) NOT NULL DEFAULT '0',
   `phone` varchar(20) COLLATE utf8_bin NOT NULL,
-  `QQ` varchar(10) COLLATE utf8_bin NOT NULL,
+  `qq` varchar(10) COLLATE utf8_bin NOT NULL,
   `email` varchar(15) COLLATE utf8_bin NOT NULL,
-  `timestamp` int(11) NOT NULL,
-  `url` varchar(300) COLLATE utf8_bin NOT NULL,
+  `submit_time` int(11) NOT NULL DEFAULT '0',
+  `domain` varchar(300) COLLATE utf8_bin NOT NULL,
   `title` varchar(300) COLLATE utf8_bin NOT NULL,
   `abstract` text COLLATE utf8_bin NOT NULL,
   `rank` int(11) NOT NULL,
   `detail` text COLLATE utf8_bin NOT NULL,
   `type` text COLLATE utf8_bin NOT NULL,
-  `fixmethod` text COLLATE utf8_bin NOT NULL
+  `fix_method` text COLLATE utf8_bin,
+  `fixed_time` int(11) NOT NULL DEFAULT '0',
+  `fixed` tinyint(1) NOT NULL DEFAULT '0',
+  `score` int(11) NOT NULL DEFAULT '0',
+  `token` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `official_reply` text COLLATE utf8_bin,
+  `note` text COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- 插入之前先把表清空（truncate） `Loophole`
+--
+
+TRUNCATE TABLE `Loophole`;
 --
 -- Indexes for dumped tables
 --
