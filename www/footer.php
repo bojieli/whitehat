4be0f366e88a401d0ee5d7ffb8f8025f80b0efdb
CUI Hao <cuihao.leo@gmail.com>
2015-05-09 22:26:02 +0800

    Change footer email

--- www/footer.php
+++ www/footer.php
@@ -1,6 +1,6 @@
 <div>
   <p>
-    <span class="text-muted">Copyright &copy; 校园白帽子安全技术挑战赛 2015</span>&nbsp;&nbsp;
-    <span class="text-muted">联系邮箱：lug@ustc.edu.cn</span>
+    <span class="text-muted">Copyleft &copy; 校园白帽子安全技术挑战赛 2015</span>&nbsp;&nbsp;
+    <span class="text-muted">联系邮箱：whitehat@ustclug.org</span>
   </p>
 </div>
