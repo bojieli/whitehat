d1adbd70417a86af3ffb02da481ae33d77825cae
Bojie Li <bojieli@gmail.com>
2015-05-13 15:59:27 +0000

    fix connect.php

--- www/connect.php
+++ www/connect.php
@@ -6,7 +6,7 @@
  * Time: 9:13 PM
  */
 try {
-    $con = new PDO('mysql:host=localhost;dbname=whitehat;charset=utf8', 'whitehat', 'whitehat');
+    $con = new PDO('mysql:host=mysql;dbname=whitehat;charset=utf8', 'whitehat', 'whitehat');
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     print "Connection failed: " . $e->getMessage() . "</br>";
